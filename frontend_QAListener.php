cacd215f749c44f8e05d6bb2bfcd01bde07782a4
uzairhafeez <muh3@njit.edu>
2017-05-06 11:04:30 -0400

    initial commit

---
+++ frontend_QAListener.php
@@ -0,0 +1,34 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function deployFrontendPackage($request)
+{
+	echo "\nRecieved Request: Installing backend package...\n";
+	shell_exec('sh install_frontend.sh'); 
+	echo "Success!\n";
+}
+
+function requestProcessor($request)
+{
+  echo "Request Received".PHP_EOL;
+  var_dump($request);
+  echo '\n' . 'End Message';
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "FEqa":
+      return deployFrontendPackage($request);	
+  }
+}
+
+$server = new rabbitMQServer("deployRabbitServer.ini","frontendQA");
+
+$server->process_requests('requestProcessor');
+exit();
+?>
